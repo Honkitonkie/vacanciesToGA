baf70156085adad3d1a4c3974fede15cfb11412e
Honkitonkie <kooyman.d@gmail.com>
2021-01-18 23:26:46 +0100

    events transpire everyday

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var CronJob = require('cron').CronJob;
 
   const doSomething = new CronJob(
-    '30 23 * * 0-6', //cron time
+    '26 23 * * 0-6', //cron time
     goDoIt, //replace with your function that you want to call
     null, //oncomplete
     false, //start flag
