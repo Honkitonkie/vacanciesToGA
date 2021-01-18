39cd5f21f0679ba3797e2b4e373ce495cdc65fd1
Honkitonkie <kooyman.d@gmail.com>
2021-01-18 22:39:48 +0100

    test cron job p/s

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+var CronJob = require('cron').CronJob;
+let minute;
 
-var schedule = require('node-schedule');
-var j = schedule.scheduleJob('17 * * *', function(){
-  const puppetLauncher = require ('./puppetLauncher');
-  console.log('Events transpire every day...');
-  puppetLauncher.puppetLauncher
-});
+if (minute==undefined){
+  minute = 33;
+}
 
+  const doSomething = new CronJob(
+    '0-59 * * * *', //cron time
+    goDoIt, //replace with your function that you want to call
+    null, //oncomplete
+    false, //start flag
+    'Europe/Amsterdam',// timezone
+    );
+    
+  function goDoIt() {
+      // const puppetLauncher = require ('./puppetLauncher');
+      console.log('Events transpire every day...');
+      // puppetLauncher.puppetLauncher
+      minute = minute + 2;
+    }
+
+  doSomething.start()  
