83225f313989b3caac8c0c4bfdb80c51a7a2c16d
Honkitonkie <kooyman.d@gmail.com>
2021-01-18 23:58:09 +0100

    try heroku-schedular

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var CronJob = require('cron').CronJob;
+// var CronJob = require('cron').CronJob;
 
-  const doSomething = new CronJob(
-    '26 23 * * 0-6', //cron time
-    goDoIt, //replace with your function that you want to call
-    null, //oncomplete
-    false, //start flag
-    'Europe/Amsterdam',// timezone
-    );
+//   const doSomething = new CronJob(
+//     '26 23 * * 0-6', //cron time
+//     goDoIt, //replace with your function that you want to call
+//     null, //oncomplete
+//     false, //start flag
+//     'Europe/Amsterdam',// timezone
+//     );
     
   function goDoIt() {
       const puppetLauncher = require ('./puppetLauncher');
       console.log('Events transpire every day...');
       puppetLauncher.puppetLauncher
     }
-
-  doSomething.start()  
+    goDoIt()
+  // doSomething.start()  
