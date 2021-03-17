35aa5e7e6867d9aa4bad49731351f5d9573887b3
Honkitonkie <kooyman.d@gmail.com>
2021-03-17 15:01:04 +0100

    test mailer in heroku

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
+require('dotenv').config()
 const puppetLauncher = require('./puppetLauncher');
-
+var nodemailer = require('nodemailer');
+const userr = process.env.USER
+const passs = process.env.APPKEY
 
 tellMeBoutit()
-function tellMeBoutit() {
-  puppetLauncher.puppetLauncher
+async function tellMeBoutit() {
+  let x = await puppetLauncher.puppetLauncher()
+  let vacs = x[0]
+  let kands = x[1]
+
+  if (Number(vacs) < 25) {
+    sendMail(x)
+  } else if (Number(kands) < 25) {
+    sendMail(x)
+  }
   console.log("Events transpire every day.....");
 }
-// return;
+
+
+function sendMail(x) {
+  var mailOptions = {
+    from: process.env.USER,
+    to: 'kooyman.d@gmail.com',
+    subject: 'De vacatures en/of kandidaten zijn onder het gewenste aantal',
+    text: 'De tellingen zijn binnen en er staan nu ' + x[0] + " vacactures en " + x[1] + " kandidaten live op de website."
+  };
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: userr,
+      pass: passs
+    }
+  });
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
--- puppetLauncher.js
+++ puppetLauncher.js
@@ -1,20 +1,43 @@
-const puppeteer = require ('puppeteer');
-const scrape = require ('./scrapeKeser');
+const puppeteer = require('puppeteer');
+const scrape = require('./scrapeKeser');
+let hoeveelVacatures
+let hoeveelKandidaten
 
-const puppetLauncher = puppeteer
-  .launch ({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']})
-  .then (async browser => {
-    let hoeveelVacatures = await scrape.vacatureNummer;
-    let hoeveelKandidaten = await scrape.kandidatenNummer;
-    const url = 'https://keser.nl/hoeveelvacatures?hoeveelVacatures='+Number(hoeveelVacatures)+'&hoeveelKandidaten='+Number(hoeveelKandidaten)+'&utm_source=DailyEvent&utm_medium=DailyEvent';
-    const page = await browser.newPage ();
-    await page.goto (url);
-    await page.waitForSelector ('body');
+const puppetLauncher = function puppetLauncher() {
+  return new Promise(resolve => {
+    try {
+
+      let pp = []
+      puppetLauncher1
+
+      setTimeout(function () {
+        let VacKan = [hoeveelVacatures, hoeveelKandidaten]
+        resolve(VacKan)
+      }, 25000);
+    }
+    catch (err) {
+      console.error("hij roept puppeteer niet", err);
+    }
+
+  }, reject => {
+    console.log("the callback to start() was rejected");
+  });
+}
+
+const puppetLauncher1 = puppeteer
+  .launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+  .then(async browser => {
+    hoeveelVacatures = await scrape.vacatureNummer;
+    hoeveelKandidaten = await scrape.kandidatenNummer;
+    const url = 'https://keser.nl/hoeveelvacatures?hoeveelVacatures=' + Number(hoeveelVacatures) + '&hoeveelKandidaten=' + Number(hoeveelKandidaten) + '&utm_source=DailyEvent&utm_medium=DailyEvent';
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector('body');
     setTimeout(() => { browser.close(); }, 25000);
   })
   //handling any errors
-  .catch (function (err) {
-    console.error (err);
+  .catch(function (err) {
+    console.error(err);
   });
 
-    exports.puppetLauncher = puppetLauncher;
+exports.puppetLauncher = puppetLauncher;
